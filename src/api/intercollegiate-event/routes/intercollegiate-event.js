f87959de98b88c4e0a2ebda3a7ee4730d629e0d4
Ibrahim Khalil <ibrahimkhalil@Ibrahims-MacBook-Air.local>
2024-09-25 04:13:03 +0530

    intercollegiate events strapi

---
+++ src/api/intercollegiate-event/routes/intercollegiate-event.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * intercollegiate-event router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::intercollegiate-event.intercollegiate-event');
---
+++ src/api/intercollegiate-event/services/intercollegiate-event.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * intercollegiate-event service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::intercollegiate-event.intercollegiate-event');
