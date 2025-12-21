0668404e5c37e0f7fefbb8d0a4ad15782605138e
Soham Tulsyan <soham.tulsyan_ug2023@ashoka.edu.in>
2025-12-21 14:13:22 +0530

    Increased payload limit

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,18 @@ module.exports = [
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
-  'strapi::body',
+  // 'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      jsonLimit: '20mb',
+      formLimit: '20mb',
+      textLimit: '20mb',
+      formidable: {
+        maxFileSize: 20 * 1024 * 1024, // 20mb
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
