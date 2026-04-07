30daa97d52033aa1d514c5fe1fa4081661603078
Nitin S <28nitin07@gmail.com>
2026-04-07 09:30:43 +0530

    feat: manual ordering

--- src/api/apl-participant/services/apl-participant.js
+++ src/api/apl-participant/services/apl-participant.js
@@ -2,4 +2,18 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::apl-participant.apl-participant');
+module.exports = createCoreService('api::apl-participant.apl-participant', {
+  /**
+   * @param {Record<string, any>} params
+   */
+  async find(params = {}) {
+    const query = { ...params };
+
+    // Set default sorting by order field if not already specified.
+    if (!query.sort) {
+      query.sort = ['order:asc', 'id:asc'];
+    }
+
+    return super.find(query);
+  }
+});
