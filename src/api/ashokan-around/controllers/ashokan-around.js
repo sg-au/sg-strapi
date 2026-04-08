0e382c229a830a440d42894b3d284b4c90d0f83c
Soham Tulsyan <soham.tulsyan_ug2023@ashoka.edu.in>
2026-04-09 02:07:53 +0530

    ADD: Ashokan Around Collection Type

---
+++ src/api/ashokan-around/controllers/ashokan-around.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ashokan-around controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ashokan-around.ashokan-around');
