31273171831d34d4dbc335f3ec2dac925d108b39
Soham Tulsyan <soham.tulsyan_ug2023@ashoka.edu.in>
2026-07-20 12:04:16 +0530

    Induction Platform v1 collection types added

---
+++ src/api/form-response/controllers/form-response.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * form-response controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::form-response.form-response');
---
+++ src/api/form-response/routes/form-response.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * form-response router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::form-response.form-response');
